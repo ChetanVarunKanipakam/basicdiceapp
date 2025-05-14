1d81b16bb9fe274d0534078676ddb3370d65d8a7
ChetanVarunKanipakam <chetanvarun121@gmail.com>
2025-05-14 15:02:15 +0530

    completed creating a basic dice roll app with flutter

---
+++ lib/dice_roller.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class DiceRoller extends StatefulWidget{
+  State<DiceRoller> createState(){
+    return _DiceRoller();
+  }
+}
+
+class _DiceRoller extends State<DiceRoller>{
+   var activatedicenumber='assets/images/dice2.png';
+
+  void rolldice(){
+    var diceroll=Random().nextInt(6)+1;
+    setState(() {
+      activatedicenumber='assets/images/dice$diceroll.png';
+    });
+  }
+  @override
+  Widget build(context){
+    return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(activatedicenumber ,width:200),
+            SizedBox(height:20),
+            TextButton(
+              onPressed: rolldice,
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                textStyle: TextStyle(
+                  fontSize: 28,
+                )
+              ),
+              child: Text("Roll Dice"))]
+        );
+  }
+}
